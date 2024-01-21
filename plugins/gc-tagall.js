43878b146cd08f69239d5736335f6df4027fe80d
VersionGrupo3 <154380259+VersionGrupo3@users.noreply.github.com>
2024-01-21 16:17:45 +0100

    〔 𝐁𝐥𝐨𝐝𝐢𝐞 𝐁𝐨𝐭 〕

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -8,9 +8,9 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   const oi = `@𝗢𝗽𝘁𝗶𝗺𝘂𝘀.𝗩𝗲𝗻𝘁𝗮𝘀.𝗕𝗼𝘁${pesan}\n`;
   let teks = `𝗛𝗼𝗹𝗮 𝗙𝗮𝗻𝘀𝗶𝘁𝗼 𝗬𝗮 𝗟𝗲𝘃𝗮𝗻𝘁𝗮𝘁𝗲 𝗘𝘀 𝗛𝗼𝗿𝗮 𝗛𝗱𝗽🌱\n\n${oi}\n`;
   for (const mem of participants) {
-    teks += `ִ໋ღ @${mem.id.split('@')[0]}\n`;
+    teks += `ִ໋🇪🇬 @${mem.id.split('@')[0]}\n`;
   }
-  teks += `\n 〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕`;
+  teks += `\n 〔 𝐁𝐥𝐨𝐝𝐢𝐞 𝐁𝐨𝐭 〕`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
