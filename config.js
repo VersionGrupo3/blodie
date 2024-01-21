556bfe78ea776871bef88446d3f1044e0bb8d17a
VersionGrupo3 <154380259+VersionGrupo3@users.noreply.github.com>
2024-01-21 07:00:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
-["51902360712", '〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕', true], //〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕
+["51902360712", '〔 𝐁𝐥𝐨𝐝𝐢𝐞 𝐁𝐨𝐭 〕', true], //〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕
 ["525526591934"], 
 ["5492996111177"]]
 
@@ -104,11 +104,11 @@ global.moment = moment
 // [ES] > Agregate a ti, colaboradores o ayudates, aparecerá en el comando de lista de contactos.
 // [EN] > Adding yourself, collaborators or helpers will appear in the contact list command.
 global.official = [ // Agregate si eres Owner
-["51902360712", '〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕', 1], 
-["51902360712", '〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕', 1],  
-["51902360712", '〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕', 1],
-["51902360712", '〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕', 1],
-["51902360712", '〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕', 1]]
+["51902360712", '〔 𝐁𝐥𝐨𝐝𝐢𝐞 𝐁𝐨𝐭 〕', 1], 
+["51902360712", '〔 𝐁𝐥𝐨𝐝𝐢𝐞 𝐁𝐨𝐭 〕', 1],  
+["51902360712", '〔 𝐁𝐥𝐨𝐝𝐢𝐞 𝐁𝐨𝐭 〕', 1],
+["51902360712", '〔 𝐁𝐥𝐨𝐝𝐢𝐞 𝐁𝐨𝐭 ', 1],
+["51902360712", '〔 𝐁𝐥𝐨𝐝𝐢𝐞 𝐁𝐨𝐭 ', 1]]
 
 global.mail = '' // Add email
 global.desc = '' // Add short description (20 caractres max)
@@ -116,8 +116,8 @@ global.desc2 = '' // Add long description (90 caractres max) (Este parámetro se
 global.country = '' // Add country, example: 🇪🇨
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
-global.packname = "〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕"
-global.author = "〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕"
+global.packname = "〔 𝐁𝐥𝐨𝐝𝐢𝐞 𝐁𝐨𝐭 "
+global.author = "〔 𝐁𝐥𝐨𝐝𝐢𝐞 𝐁𝐨𝐭 "
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 // [ES] > CUENTAS E INFORMACIÓN DE VERSIONES DEL BOT, POR FAVOR 
@@ -125,7 +125,7 @@ global.author = "〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕"
 // [EN] > ACCOUNTS AND BOT VERSION INFORMATION, PLEASE KEEP THIS UNCHANGED, WE STRIVE DAILY TO PROVIDE YOU WITH A BOT FOR THE COMMUNITY, BE GRATEFUL
 global.vs = "1.7.0"
 global.vsJB = "2.5 (Beta)"
-global.gt = "〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕"
+global.gt = "〔 𝐁𝐥𝐨𝐝𝐢𝐞 𝐁𝐨𝐭 "
 
 global.yt = "https://www.instagram.com/optimus.ventas.bot"
 global.yt2 = "https://www.instagram.com/optimus.ventas.bot"
